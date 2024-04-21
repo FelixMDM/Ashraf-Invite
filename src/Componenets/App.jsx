0552ef8e401771065c11da3472ffb845207d14c6
felix diaz <felixmdm321@gmail.com>
2024-04-21 14:01:17 -0700

    trying to get mongodb talking to appjsx

--- src/Componenets/App.jsx
+++ src/Componenets/App.jsx
@@ -4,10 +4,32 @@ import Card from "./Card/Card.jsx"
 import Form from "./Form/Form.jsx"
 import Title from "./Title/Title.jsx"
 import { useRef } from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import "../App.css"
 
 function App() {  
 
+  const [names, setNames] = useState([])
+
+  useEffect(() => {
+    fetchNames()
+  }, [])
+
+  const fetchNames = async () => {
+    try {
+      const res = await fetch('/athletes/')
+      const data = await res.json()
+      setNames(data.map(athlete => athlete.name))
+    } catch(err) {
+      console.error('Error fetching names: ', err)
+    }
+  }
+  
+  const namesAsLIs = () => {
+    return names.map((n, index) => <li key={index}>{n}</li>)
+  }
+  
   const ref = useRef(null);
 
   const handleClick = () => {
@@ -23,6 +45,11 @@ function App() {
         <Title className="title-text" tagName="banner-subtext" text="Registration Open"/>
       </div>
       <Form ref={ref}/>
+      <h1>Test Testing</h1>
+      <ul>
+        {namesAsLIs()}
+      </ul>
+      <button onClick={fetchNames}>Update List</button>
       <Footer/>
     </>
   );
